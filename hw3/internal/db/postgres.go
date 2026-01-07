79ff6783c96680066fd0cdd322590e8f00b0db8d
nrmnco <issayevnariman@gmail.com>
2026-01-08 00:27:37 +0500

    replaced dynamic fmt errors to static errors

--- hw3/internal/db/postgres.go
+++ hw3/internal/db/postgres.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -12,13 +12,13 @@ import (
 func NewPostgresDB(ctx context.Context) (*pgxpool.Pool, error) {
 	err := godotenv.Load("../.env")
 	if err != nil {
-		return nil, fmt.Errorf("error loading .env file: %w", err)
+		return nil, errors.New("error loading .env file")
 	}
 
 	connString := os.Getenv("POSTGRES")
 	pool, err := pgxpool.New(ctx, connString)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create connection pool: %w", err)
+		return nil, errors.New("failed to create connection pool")
 	}
 	return pool, nil
 }
